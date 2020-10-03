e7eb7e10a5cb3b63a88567040f04a4af0ad08348
Keith <keithtmccartney@hotmail.com>
2020-10-03 16:36:28 +0100

    Adding an AppUser class inheriting properties from the IdentityUser class.

---
+++ FullStackJobs/FullStackJobs.AuthServer.Infrastructure/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FullStackJobs.AuthServer.Infrastructure
+{
+    public class AppUser : IdentityUser
+    {
+        // Add additional profile data for application users
+        // by adding properties to this class
+        public string FullName { get; set; }
+    }
+}
