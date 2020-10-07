5c67577765d8b4be77cf35a3fc351983a300c6f9
Keith <keithtmccartney@hotmail.com>
2020-10-07 16:57:48 +0100

    Applying changes to the Account Controller, adding Signup Request & Response Models, further changed still needed so following the tutorial as it comes.

---
+++ FullStackJobs/FullStackJobs.AuthServer/Controllers/AccountsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FullStackJobs.AuthServer.Infrastructure.Data.Identity;
+using FullStackJobs.AuthServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullStackJobs.AuthServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        [Route("api/[controller]")]
+        public async Task<IActionResult> Post([FromBody]SignupRequest model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new AppUser { UserName = model.Email, FullName = model.FullName, Email = model.Email };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("userName", user.UserName));
+            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("name", user.FullName));
+            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("email", user.Email));
+            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("role", model.Role));
+
+            return Ok(new SignupResponse(user, model.Role));
+        }
+    }
+}
---
+++ FullStackJobs/FullStackJobs.AuthServer/Models/SignupResponse.cs
@@ -0,0 +1,26 @@
+﻿using FullStackJobs.AuthServer.Infrastructure.Data.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FullStackJobs.AuthServer.Models
+{
+    public class SignupResponse
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+        public SignupResponse() { }
+
+        public SignupResponse(AppUser user, string role)
+        {
+            Id = user.Id;
+            FullName = user.FullName;
+            Email = user.Email;
+            Role = role;
+        }
+    }
+}
