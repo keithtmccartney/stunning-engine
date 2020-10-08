96a48f276d75ac10ac3e6cc6ccbc5291aa33fca4
Keith <keithtmccartney@hotmail.com>
2020-10-08 07:43:29 +0100

    Adding UserManager Identity usage to the Accounts Controller.

--- FullStackJobs/FullStackJobs.AuthServer/Controllers/AccountsController.cs
+++ FullStackJobs/FullStackJobs.AuthServer/Controllers/AccountsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using FullStackJobs.AuthServer.Infrastructure.Data.Identity;
 using FullStackJobs.AuthServer.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FullStackJobs.AuthServer.Controllers
@@ -13,6 +14,8 @@ namespace FullStackJobs.AuthServer.Controllers
     [ApiController]
     public class AccountsController : ControllerBase
     {
+        private readonly UserManager<AppUser> _userManager;
+
         [Route("api/[controller]")]
         public async Task<IActionResult> Post([FromBody]SignupRequest model)
         {
